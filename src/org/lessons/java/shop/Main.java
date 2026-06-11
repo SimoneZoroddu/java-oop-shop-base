fee3e0ffedf6c924f907f4fda099536212f708dd
SimoneZoroddu <lenviur@gmail.com>
2026-06-11 23:46:35 +0200

    Implement Prodotto class with attributes and methods; update Main class to demonstrate functionality

--- src/org/lessons/java/shop/Main.java
+++ src/org/lessons/java/shop/Main.java
@@ -2,6 +2,14 @@ package org.lessons.java.shop;
 
 public class Main {
     public static void main(String[] args) {
+        Prodotto penna = new Prodotto("Bic", "di colore molto verde", 14.52f, 22);
+
+        System.out.println(penna.getNameExtension());
+        penna.stampPrice();
+        System.out.println(penna.getPriceWithIva());
+        penna.setPrice(30);
+        penna.stampPrice();
+        System.out.println(penna.getPriceWithIva());
 
     }
 }
--- src/org/lessons/java/shop/Prodotto.java
+++ src/org/lessons/java/shop/Prodotto.java
@@ -1,5 +1,39 @@
 package org.lessons.java.shop;
 
+import java.util.Random;
+
 public class Prodotto {
- 
+
+    int code;
+    String name;
+    String description;
+    float price;
+    int iva;
+
+    Random rand = new Random();
+
+    public Prodotto(String name, String description, float price, int iva) {
+        this.code = rand.nextInt(50);
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.iva = iva;
+
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public void stampPrice() {
+        System.out.println(this.price);
+    }
+
+    public float getPriceWithIva() {
+        return this.price + ((this.price * this.iva) / 100);
+    }
+
+    public String getNameExtension() {
+        return code + "-" + name;
+    }
 }
